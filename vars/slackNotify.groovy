ba6d9f700607fc0eec284c1a51a0b693220c422a
Manoj <vaddimanojkumar1028@gmail.com>
2026-06-30 15:30:42 +0530

    Update slackNotify.groovy

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -1,13 +1,13 @@
 def call(String status) {
     if (status == "SUCCESS") {
         slackSend(
-            channel: "#your-channel",
+            channel: "#jenkinsbuildnotifications",
             color: "good",
             message: "✅ Job: ${env.JOB_NAME}\nBuild: #${env.BUILD_NUMBER}\nStatus: SUCCESS\n${env.BUILD_URL}"
         )
     } else if (status == "FAILURE") {
         slackSend(
-            channel: "#your-channel",
+            channel: "#jenkinsbuildnotifications",
             color: "danger",
             message: "❌ Job: ${env.JOB_NAME}\nBuild: #${env.BUILD_NUMBER}\nStatus: FAILURE\n${env.BUILD_URL}"
         )
